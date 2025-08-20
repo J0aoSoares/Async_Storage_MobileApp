9bee576f2015ec16cdf10807a51a5c73c927846d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 20:53:03 -0300

    Aula finalizada 8/20/25

--- App.tsx
+++ App.tsx
@@ -1,5 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import RestrictedScreen from './Screens/RestrictedScreen';
+
+
+interface HomeScreenProps {  
+  
+  navigation: any; //Define 'navigation' prop type
+}
+
+// Defining HomeScreen's app
+const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
+  const [LoggedIn, setLoggedIn] = useState(false); // State that controls login
+  const [username, setUsername] = useState(''); // State for username input storage
+
+  // Collateral effect to check if user is logged in
+  useEffect(() => {
+    AsyncStorage.getItem('username').then(value => {
+      if (value) {
+        setLoggedIn(true); // User is logged in
+        setUsername(value); // Set username from AsyncStorage
+      }
+    });
+  }, []);
+
+  // Function to handle login proccess
+  const handleLogin = async () => {
+    if (username === 'joao') {
+      await AsyncStorage.setItem('username', username); // Store username
+      setLoggedIn(true);
+      navigation.navigate('Restricted'); // Navigate to RestrictedScreen
+    } else {
+      // Show alert
+      Alert.alert(
+        'Ops, Usuário não encontrado',
+        '',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel'),
+            style: 'cancel',
+          },
+          {
+            text: 'ok',
+            onPress: () => console.log('ok'),
+          },
+        ]
+      );
+    }
+  }
+}
 
 export default function App() {
   return (
